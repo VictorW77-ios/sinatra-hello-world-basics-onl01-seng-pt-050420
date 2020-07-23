da8e462aae1b1d0a6fd19589faf7de91ad2467c9
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-23 13:56:25 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-
+  
+  get '/' do      # / is the root of the app
+    "Hello, World!"
+  end 
 
 end
